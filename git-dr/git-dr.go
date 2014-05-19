d88906a70d883abb4838ad3981836fc0e9357a77
Jonas Wiklund <jwiklund@spotify.com>
2014-05-19 17:19:23 +0200

    checkout local branch tracking master except for puppet that tracks production
    Change-Id: Ifeaf02a506ac0cc67739333c48b7d0da6f93d173

--- git-dr/git-dr.go
+++ git-dr/git-dr.go
@@ -1,5 +1,84 @@
 package main
 
+import (
+	"os"
+	"os/exec"
+	"fmt"
+	"errors"
+	"strings"
+	"flag"
+)
+
+var debug = false
+
+func remoteUrl() (string, error) {
+	cmd := exec.Command("git", "config", "--get", "remote.origin.url")
+	res, err := cmd.CombinedOutput()
+	if err != nil {
+		return "", err
+	}
+	if len(res) == 0 {
+		return "", errors.New("Not a git repository, or no origin branch")
+	}
+	return strings.Trim(string(res), " \t\n\r"), nil
+}
+
+func remoteBranch(url string) (string, error) {
+	if (strings.HasSuffix(url, "spotify-puppet")) {
+		return "production", nil
+	}
+	return "master", nil
+}
+
+func checkoutRemote(branch string) error {
+	if (debug) {
+		fmt.Println("git fetch origin")
+	}
+	err := exec.Command("git", "fetch", "origin").Run()
+	if err != nil {
+		return err
+	}
+	if (debug) {
+		fmt.Println("git checkout local-" + branch)
+	}
+	err = exec.Command("git", "checkout", "local-" + branch).Run()
+	if err != nil {
+		if (debug) {
+			fmt.Println("git checkout -t origin/" + branch + " -b local-" + branch)
+		}
+		err = exec.Command("git", "checkout", "-t", "origin/" + branch, "-b", "local-" + branch).Run()
+		if err != nil {
+			return err
+		}
+	}
+	if (debug) {
+		fmt.Println("git reset origin/" + branch)
+	}
+	return exec.Command("git", "reset", "origin/" + branch).Run()
+}
+
 func main() {
-	
+	flag.BoolVar(&debug, "debug", false, "print debug information")
+	flag.Parse()
+	url, err := remoteUrl()
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	if (debug) {
+		fmt.Println("origin url", url)
+	}
+	remote, err := remoteBranch(url)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	if (debug) {
+		fmt.Println("origin branch", remote)
+	}
+	err = checkoutRemote(remote)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
 }

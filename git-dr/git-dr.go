7725fb2322f057925e42d1f3b936f8ba834d9c47
Jonas Wiklund <jwiklund@spotify.com>
2015-11-05 14:00:59 +0100

    Add agglib-common to use upstream
    Change-Id: I2e681323429f6ec6222ca8905f1a95a75fbec23b

--- git-dr/git-dr.go
+++ git-dr/git-dr.go
@@ -9,16 +9,19 @@ import (
 )
 
 func remote(url string) (string, error) {
-	if strings.HasSuffix(url, "spotify-puppet.git") {
+	if anyOf(url, "spotify-puppet.git", "dns-data.git", "user-policy.git", "agglib-common.git") {
 		return "upstream", nil
 	}
-	if strings.HasSuffix(url, "dns-data.git") {
-	        return "upstream", nil
-        }
-	if strings.HasSuffix(url, "user-policy.git") {
-                return "upstream", nil	    
-	}
 	return "origin", nil
+}
+
+func anyOf(url string, suffixes ...string) bool {
+	for _, suffix := range suffixes {
+		if strings.HasSuffix(url, suffix) {
+			return true
+		}
+	}
+	return false
 }
 
 func checkoutRemote(branch, remote string) error {

fc856a3e80a2fb41de20f4391fef30dbe8b385a4
Jonas Wiklund <jwiklund@gmail.com>
2017-09-03 19:12:32 +0200

    Fix cmd

--- parsel/main.go
+++ parsel/main.go
@@ -1,24 +1,29 @@
 package main
 
 import (
+	"os"
+
 	"github.com/jwiklund/tools/parsel/cmd"
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
 var (
-	fromRaw    = kingpin.Flag("from", "Only include items from this time").Short('F').String()
-	toRaw      = kingpin.Flag("to", "Only include items until this time").Short('T').String()
-	delimiter  = kingpin.Flag("delimiter", "Field delimiter").Short('d').String()
-	fieldsRaw  = kingpin.Flag("fields", "Only return fields (eg 1,2,3-4)").Short('f').String()
-	filtersRaw = kingpin.Flag("filter", "Filtering to perform").Strings()
-	cpuprofile = kingpin.Flag("cpuprofile", "Write cpuprofile to file").String()
-	preview    = kingpin.Flag("preview", "Preview the result, only return 10 rows").Short('p').Bool()
-	verbose    = kingpin.Flag("verbose", "Be verbose").Short('v').Bool()
-	files      = kingpin.Flag("files", "Files to read").Required().Strings()
+	app        = kingpin.New("filter", "Filter logs")
+	fromRaw    = app.Flag("from", "Only include items from this time").Short('F').String()
+	toRaw      = app.Flag("to", "Only include items until this time").Short('T').String()
+	delimiter  = app.Flag("delimiter", "Field delimiter").Short('d').String()
+	fieldsRaw  = app.Flag("fields", "Only return fields (eg 1,2,3-4)").Short('f').String()
+	filtersRaw = app.Flag("filter", "Filtering to perform").Strings()
+	cpuprofile = app.Flag("cpuprofile", "Write cpuprofile to file").String()
+	preview    = app.Flag("preview", "Preview the result, only return 10 rows").Short('p').Bool()
+	verbose    = app.Flag("verbose", "Be verbose").Short('v').Bool()
+	files      = app.Arg("files", "Files to read").Required().Strings()
 )
 
 func main() {
-	kingpin.Parse()
+	app.HelpFlag.Short('h')
+
+	kingpin.MustParse(app.Parse(os.Args[1:]))
 
 	cmd.FromRaw = fromRaw
 	cmd.ToRaw = toRaw
